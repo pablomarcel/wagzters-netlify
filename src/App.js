ba2fec04415e022ef54529944eb71e9ffea138dd
pablomarcel <pablomarcel@outlook.com>
2023-04-23 15:41:54 -0700

    attempt to implement bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap-5.2.3-dist/css/bootstrap.min.css';
 
 function App() {
