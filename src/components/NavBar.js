1b2b1bd8039a0849527063b8509a0b9e5abb6e47
pmarcel-uw <pmarcel@uw.com>
2023-04-30 13:07:59 -0700

    attempted to update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -8,7 +9,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <Link className="navbar-brand" to="/">Wagzters</Link>
+                <Link className="navbar-brand" to="/">Home</Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         {isAuthenticated && (
